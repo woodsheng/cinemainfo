b6bbd0ae0850cd1582bbdde75e38a33f55d84682
ws <woodsheng@sohu.com>
2019-07-24 08:41:47 +0800

    git commit -m "add detailinfo"

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {
 
-  },
-  mutations: {
+    },
+    mutations: {
 
-  },
-  actions: {
+    },
+    actions: {
 
-  }
+    },
+    modules:{
+        city
+    }
 })
